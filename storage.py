047a0fcf0f8d1a30b79d6a2fd672e681eb1398ed
Luiz Orlando <128615584+LuizOrla@users.noreply.github.com>
2026-08-04 17:36:31 -0300

    Create storage.py
    Adição de arquivo que manipula gravação de dicionário, criando, lendo, alterando e apagando tarefas

---
+++ storage.py
@@ -0,0 +1,49 @@
+# Arquivo de armazenamento de projetos, com suas respectivas tarefas
+
+# Importação de classe Tarefa, e pacotes pertinentes
+from classes import Tarefa
+import config
+import json
+from json import JSONDecodeError
+import uuid
+import os
+
+# ========== Funções de Arquivos ==========
+def ler_arquivos(): # Lê os arquivos e atualiza variáveis de dicionário globais
+    try:
+        with open (config.ARQUIVO_TAREFAS, "r", encoding="utf-8") as arquivo:
+            config.dict_tarefas = json.load(arquivo)
+    except (JSONDecodeError, FileNotFoundError):
+        dict = {}
+        salvar_tarefas_arquivo(dict)  
+def salvar_tarefas_arquivo(dict_tarefa): # Atualiza o arquivo de tarefas a partir do dicionário
+    with open(config.ARQUIVO_TAREFAS, "w", encoding="utf-8") as arquivo:
+        conteudo = json.dumps(dict_tarefa, indent=4, ensure_ascii=False)
+        arquivo.write(conteudo)
+        arquivo.flush()
+        os.fsync(arquivo.fileno())
+
+# ========== Funções de Tarefas ==========
+def criar_tarefa(nome, descricao): # Cria uma nova tarefa e a adiciona no dicionário
+    id_tarefa = str(uuid.uuid4()) # Atribui um ID único à tarefa
+    tarefa = Tarefa(id_tarefa, config.projeto_ativo, nome, descricao) # Cria um objeto Tarefa com informações fornecidas
+    id_tarefa, dict_tarefa = tarefa.dicionario_tarefa() # Cria nova entrada a aprtir de objeto
+    config.dict_tarefas[id_tarefa] = dict_tarefa # Adiciona Tarefa a dicionário global
+    salvar_tarefas_arquivo(config.dict_tarefas) # Salva arquivo de Tarefas
+def avancar_tarefa(id_tarefa): # Altera o status de uma tarefa para trás, baseado em seu status atual
+    for id_task, tarefa in config.dict_tarefas.items():
+        if id_task == id_tarefa:
+            match tarefa["status"]:
+                case "todo": tarefa["status"] = "doing"
+                case "doing": tarefa["status"] = "done"
+    salvar_tarefas_arquivo(config.dict_tarefas)
+def retornar_tarefa(id_tarefa): # Altera o status de uma tarefa para trás, baseado em seu status atual
+    for id_task, tarefa in config.dict_tarefas.items():
+        if id_task == id_tarefa:
+            match tarefa["status"]:
+                case "doing": tarefa["status"] = "todo"
+                case "done": tarefa["status"] = "doing"
+    salvar_tarefas_arquivo(config.dict_tarefas)
+def apagar_tarefa(id_tarefa): # Exclui uma tarefa do dicionário
+    del config.dict_tarefas[id_tarefa]
+    salvar_tarefas_arquivo(config.dict_tarefas)
